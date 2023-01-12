55615a172d1645a4b0c9370bcf28634b902f7e15
Comrade-Sev <79182100+Comrade-Sev@users.noreply.github.com>
2023-01-12 11:22:08 +0100

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float initialPlayerSpeed = 4f;
     [SerializeField] private float maximumPlayerSpeed = 30f;
     [SerializeField] private float playerSpeedIncreaseRate = .1f;
-    [SerializeField] private float jumpheight
+    [SerializeField] private float jumpHeight = 1.0f;
+    [SerializeField] private float initialGravityValue = -9.8f;
+
+    private float playerSpeed;
+    private Vector3 movementDirection = Vector3.forward;
+    
+    
 
 }
