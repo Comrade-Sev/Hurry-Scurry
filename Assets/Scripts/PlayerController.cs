dc0c3566c821abafb08cb4fe6d2517264d9ee8bd
Comrade-Sev <79182100+Comrade-Sev@users.noreply.github.com>
2023-01-12 11:17:56 +0100

    Player Controller Script added

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float initialPlayerSpeed = 4f;
+    [SerializeField] private float maximumPlayerSpeed = 30f;
+    [SerializeField] private float playerSpeedIncreaseRate = .1f;
+    [SerializeField] private float jumpheight
+
+}
